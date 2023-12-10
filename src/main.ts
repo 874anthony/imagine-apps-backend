46a82a8ba269811c3e4f9cb57e1105d912cfb70b
Anthony Acosta M <anthony082008@hotmail.es>
2023-12-10 17:43:20 -0500

    Enabled cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { MongooseExceptionFilter } from './shared/filters/mongo-exception/mongoo
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new MongooseExceptionFilter());
   await app.listen(3000);
